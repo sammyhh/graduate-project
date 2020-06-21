e76ec8da918d8857e835ffa0d9bb2c1c047c256e
sammyhh <hyunjhwag@gmail.com>
2020-06-20 20:01:09 -0700

    initial add

---
+++ excelread.py
@@ -0,0 +1,23 @@
+import random
+import string
+from openpyxl import load_workbook
+import pyperclip
+import names
+
+
+
+fn = names.get_first_name()
+print(fn)
+ln = names.get_last_name()
+
+wb = load_workbook('nike.xlsx')
+data = wb['Sheet1']
+sheet = wb.active
+
+
+last_row = sheet.max_row
+while sheet.cell(column=1, row=last_row).value is None and last_row > 0:
+    last_row -= 1
+last_col_a_value = sheet.cell(column=1, row=last_row).value
+
+print(last_col_a_value)
